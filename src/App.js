413b2828997566d335292e3bee49d3fe3ed79035
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-11-18 12:58:24 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homePage/homePage';
+
+function HatsPage(props) {
+  console.log(props)
+  return <h1>Hat Page</h1>
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+
     </div>
   );
 }
--- src/components/menuItem/menuItem.jsx
+++ src/components/menuItem/menuItem.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-
+import { useNavigate } from "react-router-dom";
 import './menuItem.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={size ? `large menu-item` : 'menu-item'}>
-        <div
-            className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+function MenuItem({ title, imageUrl, size }) {
+    let navigate = useNavigate();
+    return (
+        <div className={size ? `large menu-item` : 'menu-item'}>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle' onClick={() => navigate("hats")}>SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    )
+}
 
 export default MenuItem;
