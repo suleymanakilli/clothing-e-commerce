9b1187aff523cf0da9952e5b42e601e70ca8472c
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-11-25 21:15:05 +0300

    updated shop data updated cart utils to group items

--- src/components/collectionPreview/collectionPreview.jsx
+++ src/components/collectionPreview/collectionPreview.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './collectionPreview.scss'
 import CollectionItem from '../collectionItem/collectionItem'
-export default function CollectionPreview({title,items}) {
+export default function CollectionPreview({ title, items }) {
 
     return (
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {items.filter((item,idx)=>idx<4).map(({id,...otherItemProps})=>(
-                    <CollectionItem key={id} {...otherItemProps}/>
+                {items.filter((item, idx) => idx < 4).map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))}
             </div>
         </div>
--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -2,3 +2,7 @@ import CartActionTypes from "./cartTypes"
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN,
 })
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
