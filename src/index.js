a30ffc0dd9e1dcd830e203dc22e1753af270e7ca
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-11-27 07:51:32 +0300

    redux persist added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store'
+
+
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
     </BrowserRouter>
   </Provider>
   ,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //Local storage
+//sessionStorage ile sessionStorage kullanabilirdik
 import userReducer from "./user/userReducer";
 import cartReducer from './cart/cartReducer'
-export default combineReducers({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] //Sadece cartlisti ekle. (User firebase auth tarafından tutuluyor. Bizim tutmamıza gerek yok)
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
