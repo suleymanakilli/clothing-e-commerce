3a70d5f5d516915268360693840edef8021c1e18
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-11-29 11:57:43 +0300

    refactored

--- src/pages/collection/collectionPage.jsx
+++ src/pages/collection/collectionPage.jsx
@@ -6,11 +6,12 @@ import './collectionPage.scss'
 
 
 function CollectionPage({ collection }) {
+    const { title, items } = collection
     return (
         <div className="collection-page">
-            <h2 className="title">{collection?.title}</h2>
+            <h2 className="title">{title}</h2>
             <div className="items">
-                {collection?.items.map(item => (
+                {items.map(item => (
                     <CollectionItem key={item.id} item={item} />
                 ))}
             </div>
