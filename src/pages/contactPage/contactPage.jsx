cc2a61edc22fa8e73ab8d888fea6301aef349340
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-12-21 13:12:01 +0300

    added contact page

---
+++ src/pages/contactPage/contactPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { ContactPageContainer } from './contactPageStyle'
+function ContactPage() {
+    return (
+        <ContactPageContainer>
+            <div>
+                <span>Email : </span>
+                <span>suleymanakilli1@gmail.com</span>
+            </div>
+            <div>
+                <a href="https://github.com/suleymanakilli">Github</a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/süleyman-akıllı-b8b2231b1/">Linkedin</a>
+            </div>
+        </ContactPageContainer>
+    )
+}
+
+export default ContactPage
---
+++ src/pages/contactPage/contactPageStyle.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContactPageContainer = styled.div`
+    margin-top: 5vw;
+    text-align: center;
+    font-size:clamp(1.5rem,4vw,4rem);
+`
