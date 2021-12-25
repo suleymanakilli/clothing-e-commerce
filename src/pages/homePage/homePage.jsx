90bc0ed142159a1455dea3c3509cfa50d5203481
Süleyman Akıllı <suleymanakilli1@gmail.com>
2021-12-25 13:38:23 +0300

    Profiler for directory added

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Profiler } from 'react'
 import Directory from '../../components/directory/directory'
 import { HomePageContainer } from './homePageStyle'
 export default function homePage() {
     return (
         <HomePageContainer>
-            <Directory />
+            <Profiler id="Directory" onRender={(id, phase, actualDuration) => {
+                console.log({
+                    id,
+                    phase,
+                    actualDuration
+                })
+            }}>
+                <Directory />
+            </Profiler>
+
         </HomePageContainer>
     )
 }
